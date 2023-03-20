679624c05b77a01974f16bd584fb65054df23be8
nathanmeade <nathan.r.meade@gmail.com>
2023-03-20 14:28:35 -0400

    Testing the inclusion of the ConnectionErrorJoke project locally using a new text view

--- app/src/main/java/com/meadetechnologies/githubactionstestapp/MainActivity.kt
+++ app/src/main/java/com/meadetechnologies/githubactionstestapp/MainActivity.kt
@@ -3,12 +3,15 @@ package com.meadetechnologies.githubactionstestapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.meadetechnologies.connectionerrorjoke.ConnectionErrorJoke
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val textView = findViewById<TextView>(R.id.textView)
+        val textView2 = findViewById<TextView>(R.id.textView2)
         textView.text = BuildConfig.apiKey
+        textView2.text = ConnectionErrorJoke.getJoke()
     }
 }
