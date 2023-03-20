804acbc0a74b8b637cd60e25656c3092b0812301
nathanmeade <nathan.r.meade@gmail.com>
2023-03-19 23:38:25 -0400

    Missing files added

--- app/src/main/java/com/meadetechnologies/githubactionstestapp/MainActivity.kt
+++ app/src/main/java/com/meadetechnologies/githubactionstestapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.meadetechnologies.githubactionstestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = BuildConfig.apiKey
     }
 }
